cca5f526488d088bac64b0c4342a48c2ec5e25fc
Paweł Łaba <labapawel@gmail.com>
2025-09-30 10:49:22 +0200

    Update timezone to Europe/Warsaw and add Project, Stage, and Task models with corresponding migrations

---
+++ database/migrations/2025_09_29_072958_create_stages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->foreignId('project_id')->constrained()->onDelete('cascade');
+            $table->integer('order')->default(0); // Order of the stage in the project
+            $table->integer('duration')->nullable(); // Duration in procented project time
+            $table->boolean('active')->default(true); // active, completed, on-hold
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stages');
+    }
+};
